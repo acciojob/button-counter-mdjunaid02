21803d7f1ad807e79c40cc6fa4500b08f39558ab
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-01-28 20:04:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+function App(){
+  const[value, setValue]=useState(0);
+  return(
+   <>
+    <button onClick={()=>setValue(value+1}>
+    Click me
+    </button>
+    <p>Button clicked {value} times.</p>
+    </> 
+  )
+}
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
